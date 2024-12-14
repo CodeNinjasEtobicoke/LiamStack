e287cbdea5224a597ecf78323aef772a3e276351
Astroboy <astrobrandon2013@gmail.com>
2024-12-14 12:27:37 -0500

    YRBYTRY
    VRRRYRT

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -40,10 +40,34 @@ public class GameController : MonoBehaviour
             currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
                 currentCube.transform.position.y,
                 Mathf.Round(currentCube.transform.position.z));
-            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x)
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                            lastCube.transform.localScale.y,
+                                                           lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+
 
                 
         }
+
+        if (currentCube.transform.localScale.x <= 0f ||
+            currentCube.transform.localScale.z <= 0f)
+        {
+            Done = true;
+            text.gameObject.SetActive(true);
+            text.text = "Final Score: " + Level;
+            StartCoroutine(X());
+            return;
+
+           }
+
+        lastCube = currentCube;
+        currentCube = Instantiate(lastCube);
+        currentCube.name = Level + "";
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+        Level++;
+        Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        Camera.main.transform.LookAt(currentCube.transform.position);
+
 
 
     }
