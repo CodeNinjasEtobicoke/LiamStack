62f0bcb6b0b94170701059870c7a6a4eeea9d859
CodeNinjasEtobicoke <>
2025-01-04 12:05:32 -0500

    edwxwdwz
    dzwedqwdqedzqwd

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -38,13 +38,16 @@ public class GameController : MonoBehaviour
         var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
         var pos1 = lastCube.transform.position + Vector3.up * 10f;
         var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        var pos3 = pos1 + ((Level % 2 == 0) ? Vector3.right : Vector3.back) * 120;
+
+
         if (Level % 2 == 0)
         {
-            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+            currentCube.transform.position = Vector3.Lerp(pos2, pos3, time);
         }
         else
         {
-            currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+            currentCube.transform.position = Vector3.Lerp(pos3, pos2, time);
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -56,8 +59,7 @@ public class GameController : MonoBehaviour
 
     void Newblock()
     {
-
-        if(lastCube != null)
+        if (lastCube != null)
         {
 
             currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
@@ -81,7 +83,7 @@ public class GameController : MonoBehaviour
             StartCoroutine(X());
             return;
 
-           }
+        }
 
         lastCube = currentCube;
         currentCube = Instantiate(lastCube);
@@ -100,6 +102,5 @@ public class GameController : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         SceneManager.LoadScene("SampleScene");
-
     }
 }
